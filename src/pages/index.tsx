0ab7aca053139ab0a1e28e2f766ce9838925b07b
jovand3v <jovand3v@gmail.com>
2023-01-15 19:42:53 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>PHERRA | Italian high-end luxury fashion clothing & accessories</title>
+        <meta
+          name="description"
+          content="Discover luxury clothing & accessories by exploring PHERRA's Summer and Winter 2023 Collections. Shop in-store or online. Worldwide shipping available."
+        />
+        <link rel="icon" href="/assets/icons/favicon.ico" />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
