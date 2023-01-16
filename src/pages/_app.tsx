f45fc1aee9f4ee25aa85b8a162d9608c6edb9d60
jovand3v <jovand3v@gmail.com>
2023-01-16 09:25:54 +0100

    added a global font-family with @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { Raleway } from "@next/font/google";
+
+const raleway = Raleway({ subsets: ["latin"] });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={raleway.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default MyApp;
