4bf5a64646aa43b7302e1d7b4a622d46f6b5c313
jovand3v <jovand3v@gmail.com>
2023-04-02 19:44:31 +0200

    collections are now dynamic

--- src/components/Collections/Collection.tsx
+++ src/components/Collections/Collection.tsx
@@ -1,9 +1,7 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import s from "./Collection.module.scss";
 import CollectionProduct from "./CollectionProduct";
 import ArrowIcon from "@public/assets/icons/arrow-long-fat.svg";
-import summerThumbnail from "@public/assets/thumbnails/summer-model-2.png";
-import winterThumbnail from "@public/assets/thumbnails/winter-model-1.png";
 import Image from "next/image";
 import Searchbar from "../common/Searchbar";
 import DropdownMenu from "../common/DropdownMenu";
@@ -11,10 +9,11 @@ import Cart from "../common/Cart";
 import Link from "next/link";
 import CollectionProductInfo from "./CollectionProductInfo";
 import { Product } from "src/lib/products";
-import { products } from "src/lib/products";
+import { StaticProps } from "src/pages/collections/[collection]";
 
-const Collection = () => {
-  const [selectedProduct, setSelectedProduct] = useState<Product>(products[0]);
+const Collection = (props: StaticProps) => {
+  const { collection, products, thumbnail } = props;
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(products[0]);
   const [showcaseActive, setShowcaseActive] = useState(false);
   const [showChild, setShowChild] = useState(false);
 
@@ -30,20 +29,20 @@ const Collection = () => {
           <Link href="/">
             <ArrowIcon className={s.arrowIcon} />
           </Link>
-          {showChild && (
+          {showChild && selectedProduct && (
             <CollectionProductInfo
               selectedProduct={selectedProduct}
               showcaseActive={showcaseActive}
               setShowcaseActive={(s) => setShowcaseActive(s)}
             />
           )}
-          <Image className={s.thumbnailImage} src={summerThumbnail} alt="model" />
+          <Image className={s.thumbnailImage} src={thumbnail} alt="model" />
         </div>
       </div>
       <div className={s.mainContainer}>
         <header className={s.header}>
           <h1 className={s.title}>
-            SUMMER <br />
+            {collection} <br />
             <span className={s.titleSmall}>COLLECTION</span>
           </h1>
           <p className={s.subtitle}>2023</p>
--- src/pages/collections/[collection].tsx
+++ src/pages/collections/[collection].tsx
@@ -1,13 +1,52 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { StaticImageData } from "next/image";
 import Collection from "src/components/Collections/Collection";
 import Footer from "src/components/common/Footer";
+import { Product, products } from "src/lib/products";
+import summerThumbnail from "@public/assets/thumbnails/summer-model-2.png";
+import winterThumbnail from "@public/assets/thumbnails/winter-model-1.png";
 
-const CollectionPage: NextPage = () => {
+type Collections = "summer" | "winter";
+type StaticPaths = { params: { collection: Collections } }[];
+export type StaticProps = { collection: Collections; products: Product[]; thumbnail: StaticImageData };
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths: StaticPaths = [{ params: { collection: "summer" } }, { params: { collection: "winter" } }];
+  return {
+    paths,
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const collectionProducts = products.filter((product) => product.collection === params!.collection);
+  // this is temporarily, the thumnbail will be retrieved from the db later on
+  let thumbnail = summerThumbnail;
+  switch (params!.collection) {
+    case "summer":
+      thumbnail = summerThumbnail;
+      break;
+    case "winter":
+      thumbnail = winterThumbnail;
+      break;
+  }
+  return {
+    props: { collection: params!.collection, products: collectionProducts, thumbnail },
+  };
+};
+
+const CollectionPage: NextPage<StaticProps> = (props) => {
+  const { collection, products, thumbnail } = props;
+
+  const handleCapitalize = (collection: Collections) => {
+    return collection.charAt(0).toUpperCase() + collection.slice(1);
+  };
+  const title = `PHERRA | ${handleCapitalize(collection)} Collection 2023`;
+
   return (
     <div>
       <Head>
-        <title>PHERRA | Summer Collection 2023</title>
+        <title>{title}</title>
         <meta
           name="description"
           content="Designed to keep you cool while rocking fashion during those hot summer days by utilizing summer specific fabrics and designs."
@@ -15,7 +54,7 @@ const CollectionPage: NextPage = () => {
         <link rel="icon" href="/assets/icons/favicon.ico" />
       </Head>
       <main>
-        <Collection />
+        <Collection collection={collection} products={products} thumbnail={thumbnail} />
       </main>
       <Footer separator={false} />
     </div>
