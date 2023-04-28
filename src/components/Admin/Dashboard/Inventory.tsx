3bab80b6bea997886c22bf142ce0c08558b72e61
jovand3v <jovand3v@gmail.com>
2023-04-29 00:08:29 +0200

    admin dashboard inventory now displays real values

--- src/components/Admin/Dashboard/Inventory.tsx
+++ src/components/Admin/Dashboard/Inventory.tsx
@@ -1,19 +1,33 @@
+import { AdminDashboardCollection } from "./Collection";
 import s from "./Inventory.module.scss";
 
-const Inventory = () => {
+type Props = {
+  collections: AdminDashboardCollection[];
+};
+
+const Inventory = (props: Props) => {
+  const { collections } = props;
+  const categoryAmount: number = collections.length;
+  const productAmount: number = collections.reduce((acc, curr) => (acc += curr.products.length), 0);
+  const outOfStockAmount: number = collections.reduce((acc, curr) => {
+    const stocks = curr.products.map((product) => product.stock);
+    stocks.forEach((stockArr) => stockArr.forEach((stock) => JSON.parse(stock.quantity) === 0 && (acc += 1)));
+    return acc;
+  }, 0);
+
   return (
     <ul className={s.main}>
       <li className={s.category}>
         <p className={s.title}>CATEGORIES</p>
-        <p className={s.amount}>3</p>
+        <p className={s.amount}>{categoryAmount}</p>
       </li>
       <li className={s.category}>
         <p className={s.title}>TOTAL PRODUCTS</p>
-        <p className={s.amount}>35</p>
+        <p className={s.amount}>{productAmount}</p>
       </li>
       <li className={s.category}>
         <p className={s.title}>OUT OF STOCK</p>
-        <p className={s.amount}>6</p>
+        <p className={s.amount}>{outOfStockAmount}</p>
       </li>
     </ul>
   );
