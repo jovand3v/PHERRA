271aa0726ea1485cfdfe6757dbb9d8b627ea2306
jovand3v <jovand3v@gmail.com>
2023-05-11 02:09:00 +0200

    added login behaviour for admin

--- src/components/Admin/Login/index.tsx
+++ src/components/Admin/Login/index.tsx
@@ -3,9 +3,26 @@ import s from "./index.module.scss";
 import UserIcon from "@public/assets/icons/user.svg";
 import EyeOpenIcon from "@public/assets/icons/eye-open.svg";
 import EyeClosedIcon from "@public/assets/icons/eye-closed.svg";
+import Router from "next/router";
 
 const Login = () => {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
+  const [details, setDetails] = useState({ name: "", password: "" });
+
+  const handleSubmit = async () => {
+    fetch("/api/login", {
+      method: "POST",
+      redirect: "follow",
+      body: JSON.stringify(details),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        if (res.redirected) {
+          Router.push("/admin/dashboard");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className={s.main}>
@@ -17,12 +34,23 @@ const Login = () => {
         <form className={s.form}>
           <label className={s.label}>
             <span className={s.labelName}>NAME</span>
-            <input className={s.input} placeholder="Your name" />
+            <input
+              className={s.input}
+              placeholder="Your name"
+              value={details.name}
+              onChange={(e) => setDetails((d) => ({ ...d, name: e.target.value }))}
+            />
           </label>
           <label className={s.label}>
             <span className={s.labelName}>PASSWORD</span>
             <div className={s.passwordInputWrapper}>
-              <input className={s.input} type={isPasswordShown ? "text" : "password"} placeholder="Your password" />
+              <input
+                className={s.input}
+                type={isPasswordShown ? "text" : "password"}
+                placeholder="Your password"
+                value={details.password}
+                onChange={(e) => setDetails((d) => ({ ...d, password: e.target.value }))}
+              />
               {isPasswordShown ? (
                 <EyeOpenIcon className={s.eyeIcon} onClick={() => setIsPasswordShown(false)} />
               ) : (
@@ -30,7 +58,9 @@ const Login = () => {
               )}
             </div>
           </label>
-          <button className={s.button}>Login</button>
+          <button className={s.button} type="button" onClick={handleSubmit}>
+            Login
+          </button>
         </form>
       </div>
     </div>
---
+++ src/pages/api/login.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import { setCookie } from "cookies-next";
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  // temp static admin until admins db table is created
+  const admin = { id: 1, name: "admin", password: "admin" };
+  if (req.body.name === admin.name && req.body.password === admin.password) {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      res.status(404).json({ error: "jwt secret not found" });
+    } else {
+      const jwtToken = jwt.sign({ sub: admin.id, name: admin.name, admin: true }, secret, { expiresIn: "1d" });
+      setCookie("jwt_token", jwtToken, {
+        req,
+        res,
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24,
+      });
+      res.redirect(302, "/admin/dashboard");
+    }
+  }
+};
+
+export default handler;
